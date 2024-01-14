07831f4ecdac2aa8178bc3a4c9e2102b03924068
Kyslenko-Roma <112819019+Kyslenko-Roma@users.noreply.github.com>
2024-01-14 23:21:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let yourName = prompt("Введіть імя");
+alert("привіт " + yourName);
+
+let yourAge = prompt("Введіть вік");
+alert("тобі " + yourAge);
+
+let sumNumber1 = +prompt("Введіть число");
+let sumNumber2 = +prompt("Введіть число");
+let productNumber = alert(sumNumber1 + sumNumber2);
